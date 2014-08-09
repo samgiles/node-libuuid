646ce12c6549fb12315ec1c19be81297c087aad0
SamGiles <samuel.giles@ft.com>
2014-08-09 19:48:01 +0100

    Added generate_random

--- bench/test.js
+++ bench/test.js
@@ -4,8 +4,8 @@ var nodeuuid = require('node-uuid');
 
 var suitev4 = new Benchmark.Suite;
 
-suitev4.add('node-libuuid#generate', function() {
-	libuuid.generate();
+suitev4.add('node-libuuid#generate_random', function() {
+	libuuid.v4();
 }).add('node-uuid#v4', function() {
 	nodeuuid.v4();
 }).on('cycle', function(event) {
--- lib/libuuid.cc
+++ lib/libuuid.cc
@@ -22,9 +22,19 @@ Handle<Value> GenerateTime(const Arguments& args) {
 	return scope.Close(String::New(uuid_string));
 }
 
+Handle<Value> GenerateRandom(const Arguments& args) {
+	HandleScope scope;
+    unsigned char uuid[53];
+	char* uuid_string = (char*)(uuid + 16);
+	uuid_generate_random(uuid);
+	uuid_unparse(uuid, uuid_string);
+	return scope.Close(String::New(uuid_string));
+}
+
 void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("generate"), FunctionTemplate::New(Generate)->GetFunction());
 	exports->Set(String::NewSymbol("generate_time"), FunctionTemplate::New(Generate)->GetFunction());
+	exports->Set(String::NewSymbol("generate_random"), FunctionTemplate::New(GenerateRandom)->GetFunction());
 }
 
 NODE_MODULE(node_libuuid, init)
