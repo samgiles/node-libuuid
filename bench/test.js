4f003b32f627f2cd88f0d0905dd34fb74d242a73
SamGiles <samuel.giles@ft.com>
2014-08-09 19:33:34 +0100

    Add uuid v1 api (generate_time)

--- bench/test.js
+++ bench/test.js
@@ -2,12 +2,25 @@ var Benchmark = require('benchmark');
 var libuuid = require('../lib');
 var nodeuuid = require('node-uuid');
 
-var suite = new Benchmark.Suite;
+var suitev4 = new Benchmark.Suite;
 
-suite.add('node-libuuid#generate', function() {
+suitev4.add('node-libuuid#generate', function() {
 	libuuid.generate();
 }).add('node-uuid#v4', function() {
 	nodeuuid.v4();
+}).on('cycle', function(event) {
+	  console.log(String(event.target));
+})
+.on('complete', function() {
+	  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+}).run();
+
+var suitev1 = new Benchmark.Suite;
+
+suitev1.add('node-libuuid#v1', function() {
+	libuuid.v1();
+}).add('node-uuid#v1', function() {
+	nodeuuid.v1();
 }).on('cycle', function(event) {
 	  console.log(String(event.target));
 })
--- lib/libuuid.cc
+++ lib/libuuid.cc
@@ -13,8 +13,18 @@ Handle<Value> Generate(const Arguments& args) {
     return scope.Close(String::New(uuid_string));
 }
 
+Handle<Value> GenerateTime(const Arguments& args) {
+	HandleScope scope;
+    unsigned char uuid[53];
+	char* uuid_string = (char*)(uuid + 16);
+	uuid_generate_time(uuid);
+	uuid_unparse(uuid, uuid_string);
+	return scope.Close(String::New(uuid_string));
+}
+
 void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("generate"), FunctionTemplate::New(Generate)->GetFunction());
+	exports->Set(String::NewSymbol("generate_time"), FunctionTemplate::New(Generate)->GetFunction());
 }
 
 NODE_MODULE(node_libuuid, init)
