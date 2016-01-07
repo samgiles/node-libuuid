be26305b3898ea68d1ec1274ccb2b293a1b1deac
Sam Giles <sam.e.giles@gmail.com>
2016-01-07 17:41:23 +0000

    Update to latest Node addon/v8 API

--- lib/libuuid.cc
+++ lib/libuuid.cc
@@ -4,37 +4,40 @@
 
 using namespace v8;
 
-Handle<Value> Generate(const Arguments& args) {
-    HandleScope scope;
+void Generate(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
     unsigned char uuid[53];
 	char* uuid_string = (char*)(uuid + 16);
     uuid_generate(uuid);
     uuid_unparse(uuid, uuid_string);
-    return scope.Close(String::New(uuid_string));
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, uuid_string));
 }
 
-Handle<Value> GenerateTime(const Arguments& args) {
-	HandleScope scope;
+void GenerateTime(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
     unsigned char uuid[53];
 	char* uuid_string = (char*)(uuid + 16);
 	uuid_generate_time(uuid);
 	uuid_unparse(uuid, uuid_string);
-	return scope.Close(String::New(uuid_string));
+	args.GetReturnValue().Set(String::NewFromUtf8(isolate, uuid_string));
 }
 
-Handle<Value> GenerateRandom(const Arguments& args) {
-	HandleScope scope;
+void GenerateRandom(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
     unsigned char uuid[53];
 	char* uuid_string = (char*)(uuid + 16);
 	uuid_generate_random(uuid);
 	uuid_unparse(uuid, uuid_string);
-	return scope.Close(String::New(uuid_string));
+	args.GetReturnValue().Set(String::NewFromUtf8(isolate, uuid_string));
 }
 
-void init(Handle<Object> exports) {
-    exports->Set(String::NewSymbol("generate"), FunctionTemplate::New(Generate)->GetFunction());
-	exports->Set(String::NewSymbol("generate_time"), FunctionTemplate::New(Generate)->GetFunction());
-	exports->Set(String::NewSymbol("generate_random"), FunctionTemplate::New(GenerateRandom)->GetFunction());
+void init(Local<Object> exports) {
+    NODE_SET_METHOD(exports, "generate", Generate);
+    NODE_SET_METHOD(exports, "generate_time", GenerateTime);
+    NODE_SET_METHOD(exports, "generate_random", GenerateRandom);
 }
 
 NODE_MODULE(node_libuuid, init)
